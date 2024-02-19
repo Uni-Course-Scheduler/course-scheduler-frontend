673b26e343713eecc0074c11490b2b99f5af368f
Marc Klose <marcklose99@gmail.com>
2024-02-19 10:58:26 +0100

    [ADD] Working draggable lists with modules

--- src/views/ModuleView.vue
+++ src/views/ModuleView.vue
@@ -5,82 +5,70 @@ import {useI18n} from "vue-i18n";
 import type {Ref} from "vue";
 import {onMounted, ref} from "vue";
 import {useModulesStore} from "@/stores/ModulesStore";
-import type {Module as ModuleType} from "@/models/classes/Module";
-import FetchModulesService from "@/services/FetchModulesService";
-import type {IModule} from "@/models/interfaces/IModule";
 
-const {t} = useI18n()
-const modulesStore = useModulesStore()
-const dragging: Ref<boolean> = ref(true)
-
-const allModules: Ref<IModule[]> = ref([])
+const {t} = useI18n();
+const modulesStore = useModulesStore();
+const dragging: Ref<boolean> = ref(true);
 
 onMounted(() => {
+  modulesStore.fetchModules(1);
 })
-
-
-const list1 = ref([
-  {name: "John"},
-  {name: "Joao"},
-  {name: "Jean"},
-  {name: "Gerard"}
-])
-const list2 = ref([
-  {name: "Juan"},
-  {name: "Edgard"},
-  {name: "Johnson"}
-])
-
 </script>
 
 <template>
   <main>
-    <div class="container">
-
-
       <ModulesViewHeader/>
       <div class="flex-container">
         <div class="flex-column">
           <h2>{{ t('modulesView.headings.allModules') }}</h2>
           <draggable
-              class="wrapper dashed" style="border-color: #C0C0C2"
-              v-model="list1"
+              class="wrapper dashed grow" style="border-color: #C0C0C2"
+              v-model="modulesStore.allModules"
               group="modules"
               @start="dragging=true"
               @end="dragging=false"
               item-key="id">
             <template #item="{element}">
-              <div>{{ element.name }}</div>
+              <div>{{ element.title }}</div>
             </template>
           </draggable>
         </div>
         <div class="flex-column">
           <h2>{{ t('modulesView.headings.selectedModules') }}</h2>
           <draggable
-              class="wrapper dashed" style="border-color: #9AAAEC"
-              v-model="list2"
+              class="wrapper dashed grow" style="border-color: #9AAAEC"
+              v-model="modulesStore.selectedModules"
               group="modules"
               @start="dragging=true"
               @end="dragging=false"
               item-key="id">
             <template #item="{element}">
-              <div>{{ element.name }}</div>
+              <div>{{ element.title }}</div>
             </template>
           </draggable>
         </div>
       </div>
-    </div>
   </main>
 </template>
 
 <style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+}
 .flex-container {
+  height: 100%;
   display: flex;
   column-gap: var(--column-gap-l);
 }
 
 .flex-column {
-  height: 100%;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+}
+
+.grow {
   flex: 1;
 }
 </style>
