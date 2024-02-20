2b52ef3851564419fa076999c359a414c486de6e
Marc Klose <marcklose99@gmail.com>
2024-02-20 10:02:12 +0100

    [ADD] checkMove method to prevent duplicates in selected modules

--- src/views/ModuleView.vue
+++ src/views/ModuleView.vue
@@ -1,53 +1,71 @@
 <script setup lang="ts">
 import ModulesViewHeader from "@/components/modules-view/ModulesViewHeader.vue";
-import draggable from 'vuedraggable'
-import {useI18n} from "vue-i18n";
-import type {Ref} from "vue";
-import {onMounted, ref} from "vue";
-import {useModulesStore} from "@/stores/ModulesStore";
+import draggable from "vuedraggable";
+import { useI18n } from "vue-i18n";
+import type { Ref } from "vue";
+import { onMounted, ref } from "vue";
+import { useModulesStore } from "@/stores/ModulesStore";
 
-const {t} = useI18n();
+const { t } = useI18n();
 const modulesStore = useModulesStore();
 const dragging: Ref<boolean> = ref(true);
 
+/**
+ * Prevents duplicates in selected modules
+ * @param event
+ */
+const checkMove = (event: any) => {
+  const isAlreadySelected = modulesStore.selectedModules.includes(
+    event.draggedContext.element
+  );
+  if (isAlreadySelected) {
+    return false;
+  }
+};
 onMounted(() => {
   modulesStore.fetchModules(1);
-})
+});
 </script>
 
 <template>
   <main>
-      <ModulesViewHeader/>
-      <div class="flex-container">
-        <div class="flex-column">
-          <h2>{{ t('modulesView.headings.allModules') }}</h2>
-          <draggable
-              class="wrapper dashed grow" style="border-color: #C0C0C2"
-              v-model="modulesStore.allModules"
-              group="modules"
-              @start="dragging=true"
-              @end="dragging=false"
-              item-key="id">
-            <template #item="{element}">
-              <div>{{ element.title }}</div>
-            </template>
-          </draggable>
-        </div>
-        <div class="flex-column">
-          <h2>{{ t('modulesView.headings.selectedModules') }}</h2>
-          <draggable
-              class="wrapper dashed grow" style="border-color: #9AAAEC"
-              v-model="modulesStore.selectedModules"
-              group="modules"
-              @start="dragging=true"
-              @end="dragging=false"
-              item-key="id">
-            <template #item="{element}">
-              <div>{{ element.title }}</div>
-            </template>
-          </draggable>
-        </div>
+    <ModulesViewHeader />
+    <div class="flex-container">
+      <div class="flex-column">
+        <h2>{{ t("modulesView.headings.allModules") }}</h2>
+        <draggable
+          :sort="false"
+          :group="{ name: 'modules', pull: 'clone', put: 'false' }"
+          :move="checkMove"
+          class="wrapper dashed grow"
+          style="border-color: #c0c0c2"
+          v-model="modulesStore.allModules"
+          @start="dragging = true"
+          @end="dragging = false"
+          item-key="id"
+        >
+          <template #item="{ element }">
+            <div>{{ element.title }}</div>
+          </template>
+        </draggable>
       </div>
+      <div class="flex-column">
+        <h2>{{ t("modulesView.headings.selectedModules") }}</h2>
+        <draggable
+          :group="{ name: 'modules' }"
+          class="wrapper dashed grow"
+          style="border-color: #9aaaec"
+          v-model="modulesStore.selectedModules"
+          @start="dragging = true"
+          @end="dragging = false"
+          item-key="id"
+        >
+          <template #item="{ element }">
+            <div>{{ element.title }}</div>
+          </template>
+        </draggable>
+      </div>
+    </div>
   </main>
 </template>
 
