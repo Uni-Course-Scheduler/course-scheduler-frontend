b9a54f1f74d5ef3c5e6b67ccd4efe1d4bb24ddaa
Marc Klose <marcklose99@gmail.com>
2024-02-22 15:17:30 +0100

    [REFACTOR] Removed static data and fetchModulesBySemester retrieves data by request now

--- src/services/FetchModulesService.ts
+++ src/services/FetchModulesService.ts
@@ -1,111 +1,28 @@
-import {WeekDayEnum} from "@/models/enums/WeekDayEnum";
-import {ModuleFormatEnum} from "@/models/enums/ModuleFormatEnum";
-import {ModuleTypeEnum} from "@/models/enums/ModuleTypeEnum";
+import type { AxiosResponse } from "axios";
+
+
+
 import type {IModule} from "@/models/interfaces/IModule";
+import axios from "axios";
 
 export default {
 
     /**
      *
      */
-    fetchModulesBySemester(semester: number): IModule[] {
-        return [
-            {
-                title: 'B1.6 Mathematik',
-                format: [
-                    {
-                        section: 1,
-                        title: 'B1.6 Mathematik (SL)',
-                        day: WeekDayEnum.SA,
-                        time: '09:45 bis 13:30',
-                        rhythm: 'weekly',
-                        duration: '07.10.2023 bis 03.02.2024',
-                        type: ModuleFormatEnum.SL
-                    },
-                    {
-                        section: 2,
-                        title: 'B1.6 Mathematik (BÜ)',
-                        day: WeekDayEnum.TH,
-                        time: '13:45 bis 15:15',
-                        rhythm: 'weekly',
-                        duration: '07.10.2023 bis 03.02.2024',
-                        type: ModuleFormatEnum.BÜ
-                    },
-                ],
-                type: ModuleTypeEnum.P
-            },
-            {
-                title: 'Grundlagen des Software-Engineering',
-                format: [
-                    {
-                        section: 1,
-                        title: 'B1.6 Mathematik (SL)',
-                        day: WeekDayEnum.SA,
-                        time: '09:45 bis 13:30',
-                        rhythm: 'weekly',
-                        duration: '07.10.2023 bis 03.02.2024',
-                        type: ModuleFormatEnum.SL
-                    },
-                    {
-                        section: 2,
-                        title: 'B1.6 Mathematik (BÜ)',
-                        day: WeekDayEnum.TH,
-                        time: '13:45 bis 15:15',
-                        rhythm: 'weekly',
-                        duration: '07.10.2023 bis 03.02.2024',
-                        type: ModuleFormatEnum.BÜ
-                    },
-                ],
-                type: ModuleTypeEnum.P
-            },
-            {
-                title: 'Grundlagen des Software-Engineering',
-                format: [
-                    {
-                        section: 1,
-                        title: 'B1.6 Mathematik (SL)',
-                        day: WeekDayEnum.SA,
-                        time: '09:45 bis 13:30',
-                        rhythm: 'weekly',
-                        duration: '07.10.2023 bis 03.02.2024',
-                        type: ModuleFormatEnum.SL
-                    },
-                    {
-                        section: 2,
-                        title: 'B1.6 Mathematik (BÜ)',
-                        day: WeekDayEnum.TH,
-                        time: '13:45 bis 15:15',
-                        rhythm: 'weekly',
-                        duration: '07.10.2023 bis 03.02.2024',
-                        type: ModuleFormatEnum.BÜ
-                    },
-                ],
-                type: ModuleTypeEnum.P
-            },
-            {
-                title: 'Einführung in die Wirtschaftsinformatik',
-                format: [
-                    {
-                        section: 1,
-                        title: 'B1.6 Mathematik (SL)',
-                        day: WeekDayEnum.SA,
-                        time: '09:45 bis 13:30',
-                        rhythm: 'weekly',
-                        duration: '07.10.2023 bis 03.02.2024',
-                        type: ModuleFormatEnum.SL
-                    },
-                    {
-                        section: 2,
-                        title: 'B1.6 Mathematik (BÜ)',
-                        day: WeekDayEnum.TH,
-                        time: '13:45 bis 15:15',
-                        rhythm: 'weekly',
-                        duration: '07.10.2023 bis 03.02.2024',
-                        type: ModuleFormatEnum.BÜ
-                    },
-                ],
-                type: ModuleTypeEnum.P
+    async fetchModulesBySemester(semester: number): Promise<IModule[]> {
+        try {
+            const response: AxiosResponse<IModule[]> = await axios.get(`http://localhost:5050/modules?cid=1&variant=1&semester=${semester}`);
+            return response.data;
+        } catch (error) {
+            switch (error.response?.status) {
+                default:
+                    console.log(
+                        'FetchModuleService.ts no status case ' + error.response?.status
+                    );
+                    break;
             }
-        ]
+            return [];
+        }
     }
 }
