d3b50367f5df0109772ed886c72b81821d6e764b
Marc Klose <marcklose99@gmail.com>
2024-02-22 15:21:10 +0100

    [REFACTOR] Added getFormats, getElectives, parseToInstance method

--- src/stores/ModulesStore.ts
+++ src/stores/ModulesStore.ts
@@ -1,51 +1,113 @@
-import {ref, computed} from 'vue'
-import type {Ref} from 'vue'
-import {defineStore} from 'pinia'
-import {Module} from "@/models/classes/Module";
-import type {Module as ModuleType} from "@/models/classes/Module";
+import { ref } from "vue";
+import type { Ref } from "vue";
+import { defineStore } from "pinia";
+import { Module } from "@/models/classes/Module";
+import type { Module as ModuleType } from "@/models/classes/Module";
 import FetchModulesService from "@/services/FetchModulesService";
-import type {IModule} from "@/models/interfaces/IModule";
-import type {IModuleFormat} from "@/models/interfaces/IModuleFormat";
-import {ModuleFormat} from "@/models/classes/ModuleFormat";
+import type { IModule } from "@/models/interfaces/IModule";
+import type { IModuleFormat } from "@/models/interfaces/IModuleFormat";
+import { ModuleFormat } from "@/models/classes/ModuleFormat";
+import { ModuleFormatEvent } from "@/models/classes/ModuleFormatEvent";
 
-export const useModulesStore = defineStore('modulesStore', () => {
 
-        const allModules: Ref<ModuleType[]> = ref([])
-        const selectedModules: Ref<ModuleType[]> = ref([])
+export const useModulesStore = defineStore("modulesStore", () => {
 
-        function fetchModules(semester: number): void {
-            const tempModules: ModuleType[] = []
-            const fetchedModules: IModule[] = FetchModulesService.fetchModulesBySemester(semester)
-            fetchedModules.forEach((moduleObject: IModule): void => {
-                const allModuleFormats: ModuleFormat[] = []
-                moduleObject.format.forEach((moduleFormatObject: IModuleFormat): void => {
-                    const moduleFormat: ModuleFormat = new ModuleFormat(
-                        moduleFormatObject.section,
-                        moduleFormatObject.title,
-                        moduleFormatObject.day,
-                        moduleFormatObject.time,
-                        moduleFormatObject.rhythm,
-                        moduleFormatObject.duration,
-                        moduleFormatObject.type,
-                    )
-                    allModuleFormats.push(moduleFormat)
-                })
-                const module = new Module(
-                    moduleObject.id,
-                    moduleObject.title,
-                    allModuleFormats,
-                    moduleObject.type,
-                    moduleObject.status,
-                )
-                tempModules.push(module)
-            })
-            allModules.value = tempModules
-        }
+  const allModules: Ref<Module[]> = ref([]);
+  const selectedModules: Ref<Module[]> = ref([]);
 
-        return {
-            allModules,
-            selectedModules,
-            fetchModules,
-        }
-    }
-)
+  const parseToInstance = (moduleObjects: IModule[]) => {
+    const tempModules: ModuleType[] = [];
+    let electives: ModuleType[] = [];
+
+    moduleObjects.forEach((moduleObject: IModule): void => {
+      const formats = getFormats(moduleObject);
+
+      if(moduleObject.electives) {
+        electives = getElectives(moduleObject);
+      }
+
+      const module = new Module(
+        moduleObject.id,
+        moduleObject.title,
+        formats,
+        moduleObject.type,
+        moduleObject.status,
+        moduleObject.mainModuleId,
+      );
+
+
+      module.setElectives(electives);
+      tempModules.push(module);
+    });
+    return tempModules;
+  }
+
+   async function fetchModules(semester: number): Promise<void> {
+    const fetchedModules: IModule[] = await FetchModulesService.fetchModulesBySemester(semester);
+    const tempModules = parseToInstance(fetchedModules);
+    allModules.value = tempModules;
+  }
+
+  const getFormats = (moduleObject: IModule): ModuleFormat[] => {
+    const formats: ModuleFormat[] = [];
+
+    moduleObject.formats.forEach((moduleFormatObject: IModuleFormat): void => {
+      const moduleFormat: ModuleFormat = new ModuleFormat(
+        moduleFormatObject.id,
+        moduleFormatObject.title,
+        moduleFormatObject.sws,
+        moduleFormatObject.credits,
+        moduleFormatObject.language,
+        moduleFormatObject.type,
+      );
+      if(moduleFormatObject.event) {
+
+        moduleFormat.setEvent(
+          new ModuleFormatEvent(
+            moduleFormatObject.event.id,
+            moduleFormatObject.event.section,
+            moduleFormatObject.event.group,
+            moduleFormatObject.event.start_date,
+            moduleFormatObject.event.end_date,
+            moduleFormatObject.event.start_time,
+            moduleFormatObject.event.end_time,
+            moduleFormatObject.event.rhythm,
+            moduleFormatObject.event.room,
+            moduleFormatObject.event.instructor,
+            moduleFormatObject.event.weekday
+          )
+        )
+      }
+      formats.push(moduleFormat);
+    });
+
+    return formats;
+  };
+
+
+  const getElectives = (moduleObject: IModule): Module[] => {
+    const electives: Module[] = [];
+
+    moduleObject.electives.forEach((electiveObject: IModule): void => {
+        const formats = getFormats(electiveObject);
+      const elective: Module = new Module(
+        electiveObject.id,
+        electiveObject.title,
+        formats,
+        electiveObject.type,
+        electiveObject.status,
+        electiveObject.mainModuleId
+      );
+      electives.push(elective);
+    });
+
+    return electives;
+  };
+
+  return {
+    allModules,
+    selectedModules,
+    fetchModules,
+    parseToInstance,
+  };
+});
