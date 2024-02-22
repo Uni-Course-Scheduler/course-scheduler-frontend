2a0b32223f885fd2921d9fbad57e7a81091c0469
Marc Klose <marcklose99@gmail.com>
2024-02-22 15:01:15 +0100

    [ADD] Disable prop and v-model on model variable

--- src/components/widgets/SearchInput.vue
+++ src/components/widgets/SearchInput.vue
@@ -3,13 +3,19 @@ defineProps({
   placeholder: {
     type: String,
     default: ''
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
   }
 })
+
+const model = defineModel();
 </script>
 
 <template>
   <div class="input-wrapper">
-    <input type="text" :placeholder="placeholder" />
+    <input v-model="model" :disabled="disabled" type="text" :placeholder="placeholder" />
     <slot name="icon" />
   </div>
 </template>
@@ -17,6 +23,5 @@ defineProps({
 <style scoped>
 .input-wrapper {
   position: relative;
-  margin-bottom: 1.563rem;
 }
 </style>
